5640b12d42d78aed38df4f69833511fca8bbedf3
nakaan <1011.teaflower@gmail.com>
2026-06-16 01:34:14 +0900

    ルーレットゲームの2/5構成を作成

--- roulette/script.js
+++ roulette/script.js
@@ -1,1 +1,17 @@
-console.log("Dream Roulette Start");
+const canvas = document.getElementById("rouletteCanvas");
+
+const rect = canvas.getBoundingClientRect();
+const radius = canvas.width * 0.45;
+
+canvas.width = rect.width;
+canvas.height = rect.height;
+
+const ctx = canvas.getContext("2d");
+
+ctx.arc(
+    centerX,
+    centerY,
+    canvas.width * 0.15,
+    0,
+    Math.PI * 2
+);
